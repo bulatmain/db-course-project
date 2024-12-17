0b2c6b3ccabd22755d572fbdace5b87c42e98286
bulat <sinntexxx@gmail.com>
2024-12-17 04:18:45 +0300

    added speaker dtos and mapper. updated organzer mapper

--- src/main/java/com/bulatmain/conference/application/model/dto/map/SpeakerMapper.java
+++ src/main/java/com/bulatmain/conference/application/model/dto/map/SpeakerMapper.java
@@ -1,17 +1,17 @@
 package com.bulatmain.conference.application.model.dto.map;
 
-import com.bulatmain.conference.application.model.dto.organizer.OrganizerCreateDTO;
-import com.bulatmain.conference.application.model.dto.organizer.OrganizerDTO;
+import com.bulatmain.conference.application.model.dto.speaker.SpeakerCreateDTO;
+import com.bulatmain.conference.application.model.dto.speaker.SpeakerDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(
         componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL
 )
-public interface OrganizerMapper {
-    OrganizerDTO createDtoToDto(OrganizerCreateDTO dto);
+public interface SpeakerMapper {
+    SpeakerDTO createDtoToDto(SpeakerCreateDTO dto);
 }
+
---
+++ src/main/java/com/bulatmain/conference/application/model/dto/speaker/SpeakerDTO.java
@@ -0,0 +1,10 @@
+package com.bulatmain.conference.application.model.dto.speaker;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class SpeakerDTO {
+    String id;
+}
